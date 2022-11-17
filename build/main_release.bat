0d9f0adc1cc30060d733ad73d6df8259d38e81e8
Javier guinot <whitewholffox@gmail.com>
2022-11-17 19:54:31 +0100

    Release files created. Need to fix the pdb error

--- build/main_release.bat
+++ build/main_release.bat
@@ -12,20 +12,31 @@ REM Compilación y Enlace con librería gráfica.
 @echo  Proceso por lotes iniciado.
 @echo -----------------------------------------------------------------------
 @echo  Eliminando objetos....
-del *.obj *.pdb *.ilk
+del *.obj *.pdb *.ilk *.exe
+del ..\bin\*.obj ..\bin\*.pdb ..\bin\*.ilk ..\bin\*.exe
 @echo  Objetos eliminados
 @echo -----------------------------------------------------------------------
 
 REM COMPILER
-cl /nologo /Zi /EHs /GR- /MD -I ..\includes -I ..\deps\includes /c ..\src\esfera_3d.cc
-cl /nologo /Zi /EHs /GR- /MD -I ..\includes -I ..\deps\includes /c ..\src\mv_math.cc
-cl /nologo /Zi /EHs /GR- /MD -I ..\includes -I ..\deps\includes /c ..\src\my_window.cc
-cl /nologo /Zi /EHs /GR- /MD -I ..\includes -I ..\deps\includes /c ..\src\SDL_event_control.cc
-cl /nologo /Zi /EHs /GR- /MD -I ..\includes -I ..\deps\includes /c ..\src\solar_sistem.cc
-cl /nologo /Zi /EHs /GR- /MD -I ..\includes -I ..\deps\includes /c ..\src\render.cc
 
-REM LINKER
-cl /nologo /Zi /EHs /GR- /MD /Fe:../bin/%name%.exe *.obj /link /SUBSYSTEM:CONSOLE ..\deps\libs\Win_SDL2\x86\SDL2.lib ..\deps\libs\Win_SDL2\x86\SDL2main.lib ..\deps\libs\Win_SDL2\x86\SDL2_TTF.lib opengl32.lib shell32.lib user32.lib gdi32.lib
+REM IMGUI
+  cl /nologo /Zi /EHs /GR- /MD /W4 -I ..\deps\includes\SDL2 /c ..\deps\includes\IMGUI\*.cpp
+
+  cl /nologo /Zi /EHs /GR- /MD ^
+  -I ..\includes ^
+  -I ..\deps\includes ^
+  -I ..\deps\includes\IMGUI ^
+  -I ..\deps\includes\SDL2 ^
+  /c ..\src\*.cc ..\src\*.cc
+
+  REM LINKER
+  set "SDL2_Lib=..\deps\libs\Win_SDL2\x86\SDL2.lib"
+  set "SDL2main_Lib=..\deps\libs\Win_SDL2\x86\SDL2main.lib"
+  set "SDL2ttf_Lib=..\deps\libs\Win_SDL2\x86\SDL2_TTF.lib"
+  set "OWN_my_window_lib=..\deps\libs\Win_own\my_window_release.lib"
+  set "Libs=%SDL2_Lib% %SDL2main_Lib% %SDL2ttf_Lib% %OWN_my_window_lib%"
+
+  cl /nologo /Zi /EHs /GR- /MD /Fe:../bin/%name%.exe *.obj /link /SUBSYSTEM:CONSOLE %Libs% opengl32.lib shell32.lib user32.lib gdi32.lib
 
 @echo -----------------------------------------------------------------------
 @echo  Proceso por lotes finalizado.
