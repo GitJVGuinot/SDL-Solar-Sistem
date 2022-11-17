c8f2cbbf03115fa0ddfecf4514f3ffac30e97d2b
GitJVGuinot <whitewholffox@gmail.com>
2022-11-17 13:35:08 +0100

    .bat update for Dear Imgui

--- build/solar_sistem.bat
+++ build/solar_sistem.bat
@@ -17,17 +17,23 @@ del *.obj *.pdb *.ilk
 @echo -----------------------------------------------------------------------
 
 REM COMPILER
-cl /nologo /Zi /EHs /GR- /MDd -I ..\includes -I ..\deps\includes /c ..\src\esfera_3d.cc
-cl /nologo /Zi /EHs /GR- /MDd -I ..\includes -I ..\deps\includes /c ..\src\mv_math.cc
-cl /nologo /Zi /EHs /GR- /MDd -I ..\includes -I ..\deps\includes /c ..\src\my_window.cc
-cl /nologo /Zi /EHs /GR- /MDd -I ..\includes -I ..\deps\includes /c ..\src\SDL_event_control.cc
-cl /nologo /Zi /EHs /GR- /MDd -I ..\includes -I ..\deps\includes /c ..\src\solar_sistem.cc
-cl /nologo /Zi /EHs /GR- /MDd -I ..\includes -I ..\deps\includes /c ..\src\render.cc
-
 cl /nologo /Zi /EHs /GR- /MDd -I ..\deps\includes\SDL2 /c ..\deps\includes\IMGUI\*.cpp
 
+cl /nologo /Zi /EHs /GR- /MDd ^
+-I ..\includes ^
+-I ..\deps\includes ^
+-I ..\deps\includes\IMGUI ^
+-I ..\deps\includes\SDL2 ^
+/c ..\src\*.cc
+
+pause
+
 REM LINKER
-cl /nologo /Zi /EHs /GR- /MDd /Fe:../bin/%name%.exe *.obj /link /SUBSYSTEM:CONSOLE ..\deps\libs\Win_SDL2\x86\SDL2.lib ..\deps\libs\Win_SDL2\x86\SDL2main.lib ..\deps\libs\Win_SDL2\x86\SDL2_TTF.lib opengl32.lib shell32.lib user32.lib gdi32.lib
+set "SDL2_Lib=..\deps\libs\Win_SDL2\x86\SDL2.lib"
+set "SDL2main_Lib=..\deps\libs\Win_SDL2\x86\SDL2main.lib"
+set "SDL2ttf_Lib=..\deps\libs\Win_SDL2\x86\SDL2_TTF.lib"
+set "Libs=%SDL2_Lib% %SDL2main_Lib% %SDL2ttf_Lib%"
+cl /nologo /Zi /EHs /GR- /MDd /Fe:../bin/%name%.exe *.obj /link /SUBSYSTEM:CONSOLE %Libs% opengl32.lib shell32.lib user32.lib gdi32.lib
 
 @echo -----------------------------------------------------------------------
 @echo  Proceso por lotes finalizado.
